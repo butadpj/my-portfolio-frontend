35e959f77e7ebba9bc3e21fc22a603aa48a58a21
butadpj <butadpj@gmail.com>
2021-05-14 23:35:11 +0800

    Add About section

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
 import Head from "next/head";
 import Navbar from "../components/Navbar";
 import Home from "../components/Home";
+import About from "../components/About";
 
 const index = () => {
   return (
     <>
       <Navbar />
       <Home />
+      <About />
     </>
   );
 };
