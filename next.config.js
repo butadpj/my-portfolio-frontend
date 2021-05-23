2cdcb1953b66d10505b04245f671b4cf486278f5
butadpj <butadpj@gmail.com>
2021-05-24 05:22:19 +0800

    Add Next JS configuration file

---
+++ next.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  /* config options here */
+  env: {
+    devHost: "http://127.0.0.1:8000", // Development hostname
+    prodHost: "", // Production hostname
+  },
+  images: {
+    domains: ["127.0.0.1"],
+  },
+};
