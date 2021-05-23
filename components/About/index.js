ac34a028aefedd60f6ae66673374bde96d78c918
butadpj <butadpj@gmail.com>
2021-05-24 05:25:00 +0800

    Improve About component -- Responsive Design

--- components/About/index.js
+++ components/About/index.js
@@ -1,25 +1,37 @@
 import Image from "next/image";
 import style from "./About.module.css";
+import useGetVersion from "../../hooks/useGetVersion";
 
-const About = () => {
+const About = ({ data }) => {
+  let aboutData = useGetVersion("v1", data);
+  console.log(aboutData);
+
+  // SET DEFAULT DATA (FALL BACK) when server is down
+
+  let aboutText = aboutData
+    ? aboutData.about_text
+    : "I've spent the past 1+ year solving business problems with my passion in software development.\n \nThese days, my time is spent on improving our full-stack ecommerce app that I've been working on since last year.";
+  let displayPicture = aboutData
+    ? aboutData.display_picture
+    : "/images/picture/resume_pic_1_50.png";
+
+  let personName = aboutData ? aboutData.person_name : "Paul John Butad";
+
+  console.log(displayPicture);
+
   return (
     <section className={style.about} id="about">
+      <header className={style.header}>ABOUT ME</header>
       <main className={style.main}>
-        <header className={style.header}>ABOUT ME</header>
-        <div className={style.aboutText}>
-          I've spent the past 1+ year solving business problems with my passion
-          in software development. <br /> <br /> These days, my time is spent on
-          improving our full-stack ecommerce app that I've been working on since
-          last year
-        </div>
+        <div className={style.aboutText}>{aboutText}</div>
         <div className={style.profile}>
           <Image
-            src="/picture/resume_pic_1_50.png"
+            src={displayPicture}
             width={100}
             height={100}
             className={style.profileImg}
           />
-          <div className={style.profileName}>Paul John Butad</div>
+          <div className={style.profileName}>{personName}</div>
         </div>
       </main>
     </section>
