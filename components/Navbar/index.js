e9c7b4e2ae5ea8829fd05780dc29e786d2385d59
butadpj <butadpj@gmail.com>
2021-05-24 05:28:08 +0800

    Improve Navbar Component -- Light - Dark mode when scrolling down

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -1,20 +1,54 @@
 import { useState, useEffect, useRef } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
+import { Link, animateScroll as scroll } from "react-scroll";
+
+import useScrollPosition from "@react-hook/window-scroll";
 import Image from "next/image";
 import style from "./Navbar.module.css";
-import { ReactElement } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
-
-import { Link } from "react-scroll";
+const smoothScrollDuration = 80;
 
 const Navbar = () => {
   const [navShow, setNavShow] = useState(false);
+  const [navLogoSrc, setNavLogoSrc] = useState("/images/logo/logo-nav@2x.png");
+  const scrollY = useScrollPosition(10);
+  const navBar = useRef(null);
   const navLinks = useRef(null);
+
+  const [screenSize, setScreenSize] = useState({ w: 0, h: 0 });
+  const screenResize = () => {
+    setScreenSize({ w: window.innerWidth, h: window.innerHeight });
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", screenResize);
+
+    return () => {
+      window.removeEventListener("resize", screenResize);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (window.innerWidth >= 800) {
+      console.log(screenSize);
+    } else {
+    }
+  }, [screenSize]);
 
   const handleMouseLeave = () => {
     setNavShow(false);
   };
+
+  useEffect(() => {
+    if (scrollY >= 150) {
+      navBar.current.classList.add("navBarWhite");
+      setNavLogoSrc("/images/logo/logo-nav@2x.png");
+    } else {
+      navBar.current.classList.remove("navBarWhite");
+      setNavLogoSrc("/images/logo/logo-nav-light@2x.png");
+    }
+  }, [scrollY]);
 
   useEffect(() => {
     if (navShow) {
@@ -24,7 +58,6 @@ const Navbar = () => {
 
       return;
     }
-    console.log(navLinks.current);
     navLinks.current.classList.remove("navLinksShow");
 
     return () => {
@@ -33,15 +66,16 @@ const Navbar = () => {
   }, [navShow]);
 
   return (
-    <nav className={style.navbar}>
+    <nav className={style.navbar} ref={navBar}>
       <div className={style.navContent}>
         <Image
-          src="/logo/logo-nav-mobile@2x.png"
+          src={navLogoSrc}
           alt="navLogo"
           width={31}
           height={41}
           responsive="responsive"
           className={style.navLogo}
+          onClick={() => scroll.scrollToTop()}
         />
         <div className={`${style.navLinks} `} ref={navLinks}>
           <div className={style.sectionLinks}>
@@ -51,7 +85,7 @@ const Navbar = () => {
               spy={true}
               smooth={true}
               offset={-50}
-              duration={80}
+              duration={smoothScrollDuration}
             >
               Home
             </Link>
@@ -61,13 +95,31 @@ const Navbar = () => {
               to="about"
               spy={true}
               smooth={true}
-              offset={0}
-              duration={80}
+              offset={-50}
+              duration={smoothScrollDuration}
             >
               About
             </Link>
-            <div className="projects">Projects</div>
-            <div className="contact">Contact</div>
+            <Link
+              activeClass={style.active}
+              to="projects"
+              spy={true}
+              smooth={true}
+              offset={-80}
+              duration={smoothScrollDuration}
+            >
+              Projects
+            </Link>
+            <Link
+              activeClass={style.active}
+              to="contact"
+              spy={true}
+              smooth={true}
+              offset={-50}
+              duration={smoothScrollDuration}
+            >
+              Contact
+            </Link>
           </div>
           <div className={style.socials}></div>
           <FontAwesomeIcon
