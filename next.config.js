4a85660c5ae6a9bfa1b9749c392aec0dc515e647
butadpj <butadpj@gmail.com>
2021-11-15 22:26:51 +0800

    Add production hostname for env and images

--- next.config.js
+++ next.config.js
@@ -9,9 +9,9 @@ module.exports = withPWA({
   },
   env: {
     devHost: "http://127.0.0.1:8000", // Development hostname
-    prodHost: "", // Production hostname
+    prodHost: "https://nenetelecom.pythonanywhere.com", // Production hostname
   },
   images: {
-    domains: ["127.0.0.1"],
+    domains: ["127.0.0.1", "nenetelecom.pythonanywhere.com"],
   },
 });
