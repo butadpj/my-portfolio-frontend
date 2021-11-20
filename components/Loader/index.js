d7bb845df32477f31d5004628b45d58eb1721f46
butadpj <butadpj@gmail.com>
2021-11-20 18:21:11 +0800

    Add Portal component and render Loader with Portal

--- components/Loader/index.js
+++ components/Loader/index.js
@@ -1,13 +1,16 @@
 import Image from "next/image";
 import style from "./Loader.module.css";
+import Portal from "../Portal";
 
 const Loader = () => {
   return (
-    <main className={style.loader}>
-      <div>
-        <Image src="/images/loaders/loader-gif.gif" width={150} height={150} />
-      </div>
-    </main>
+    <Portal>
+      <main className={style.loader}>
+        <div>
+          <Image src="/images/loaders/loader-gif.gif" width={150} height={150} />
+        </div>
+      </main>
+    </Portal>
   );
 };
 
--- pages/_document.js
+++ pages/_document.js
@@ -11,6 +11,7 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head />
         <body>
+          <div id='myportal' />
           <Main />
           <NextScript />
         </body>
